7dd2c0977aa0f1f35b5be0da3486f1d818a10f2a
James Mobus <mobusj@live.com>
2019-08-06 22:26:49 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -84,8 +84,8 @@ def adding_matz
           :languages => ["C"]
         },
         :yukihiro_matsumoto => {
-        :known_for => "Ruby",
-        :languages => ["LISP", "C"]
+          :known_for => "Ruby",
+          :languages => ["LISP", "C"]
         }
      }
 end
